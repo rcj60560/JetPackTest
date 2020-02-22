bb643681100a15d0723245c978787e8a4d21899f
289759379@qq.com <609432148>
2020-02-22 16:42:19 +0800

    篮球计分器小demo 数据的绑定

--- app/src/main/java/com/luocj/jetpacktest/activity/ScoreActivity.java
+++ app/src/main/java/com/luocj/jetpacktest/activity/ScoreActivity.java
@@ -1,20 +1,34 @@
 package com.luocj.jetpacktest.activity;
 
 import android.os.Bundle;
+import android.widget.ScrollView;
 
 import com.luocj.jetpacktest.R;
+import com.luocj.jetpacktest.databinding.ActivityScoreBinding;
+import com.luocj.jetpacktest.model.ScoreModel;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
 
 /**
  * 篮球计分器的demo
  * 2020年2月22日15:40:19
  */
 public class ScoreActivity extends AppCompatActivity {
+
+    ScoreModel scoreModel;
+    ActivityScoreBinding scoreBinding;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_score);
+        scoreBinding = DataBindingUtil.setContentView(this, R.layout.activity_score);
+        scoreModel = new ViewModelProvider(this).get(ScoreModel.class);
+
+        scoreBinding.setData(scoreModel);
+        scoreBinding.setLifecycleOwner(this);
+
     }
 }
